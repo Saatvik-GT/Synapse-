2528325d2788f7fbff5ee4cd0c396662139ef588
Krissh <krisshchhabra77@gmail.com>
2026-04-07 02:47:10 +0530

    feat: integrate sidebar into global layout

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/globals.css';
 
+import Sidebar from '../components/Sidebar';
+import { useState } from 'react';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [activeTab, setActiveTab] = useState("dashboard");
+
+  return (
+    <div className="flex">
+      
+      {/* Sidebar */}
+      <Sidebar 
+        activeTab={activeTab} 
+        setActiveTab={setActiveTab} 
+      />
+
+      {/* Main Content */}
+      <div className="flex-1">
+        <Component {...pageProps} activeTab={activeTab} />
+      </div>
+
+    </div>
+  );
 }
 
 export default MyApp;
